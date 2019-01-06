99635d3f62a56153d3f0b85b98071312ddc19e08
david <david@mainmac.local>
2019-01-06 12:31:14 +0100

    Stuff

---
+++ dl/model/baseline.py
@@ -0,0 +1,20 @@
+
+
+class BaselineModel:
+
+    def __init__(self):
+        pass
+
+    def model_definition(self):
+        pass
+
+    def optimizer_definition(self):
+        pass
+
+    def fit(self, X, Y):
+        """
+
+        :param X: Full dataset
+        :param Y:
+        :return:
+        """
