9361a6f9bc3bced4b79fa33894f21ca6d4977b6d
david <david@mainmac.local>
2019-01-09 22:15:27 +0100

    Still working on tsfresh

--- dl/feature_selection/fs_tsfresh.py
+++ dl/feature_selection/fs_tsfresh.py
@@ -3,6 +3,7 @@
     https://tsfresh.readthedocs.io/en/latest/
 """
 import numpy as np
+import tsfresh
 from tsfresh import extract_features, select_features
 from tsfresh.utilities.dataframe_functions import impute
 
@@ -12,23 +13,60 @@ from dl.data_loader import import_data
 class TSFresh:
 
     def __init__(self):
-        pass
+        self.fitted = False
 
-    def transform(self, timeseries):
+    def fit(self, timeseries_X, timeseries_y):
+        """
+            This prepare function can precalculate certain features given a smaller datasample (i.e. if the entire dataset is too huge).
+            It will then keep the datasamples as is.
+
+        :param timeseries_X:
+        :param timeseries_y:
+        :return:
+        """
+
+        assert len(timeseries_X) == len(timeseries_y)
+
+        # X_tsfresh containes the extracted tsfresh features
+        self.X_tsfresh = extract_features(timeseries_X, column_id="Label", column_sort="Date", n_jobs=8)
+        impute(self.X_tsfresh)
+
+        # which are now filtered to only contain relevant features
+        #
+
+        print(self.X_tsfresh)
+
+        self.X_tsfresh_filtered = select_features(self.X_tsfresh, timeseries_y) # y --> Should be the price that should be predicted
+
+
+        # we can easily construct the corresponding settings object
+        self.kind_to_fc_parameters = tsfresh.feature_extraction.settings.from_columns(self.X_tsfresh_filtered.columns) # Previously not .columns!
+
+        self.fitted = True
+
+    def transform(self, timeseries_X, timeseries_y):
         """
 
         :param X:
         :param Y:
         :return:
         """
+        assert self.fitted
+        # Will simply
 
         # Extract the features
         # extracted_features = extract_features(timeseries, column_id="id", column_sort="time")
 
         # TODO: What should the output of this be? This depends on the input of the neural network
         # and possibly on the input on the input of other models as well
-        timeseries = timeseries.dropna() # TODO: what to do with nans!
-        extracted_features = extract_features(timeseries, column_id="Label", column_sort="Date", n_jobs=16)# feature_extraction_settings = MinimalFeatureExtractionSettings())
+        timeseries = timeseries_X.dropna() # TODO: what to do with nans!
+        extracted_features = extract_features(
+            timeseries,
+            kind_to_fc_parameters=self.kind_to_fc_parameters,
+            column_id="Label",
+            column_sort="Date",
+            n_jobs=16
+        )# feature_extraction_settings = MinimalFeatureExtractionSettings())
 
         # print("Extracted features are: ", extracted_features)
         print(extracted_features.columns)
@@ -36,7 +74,6 @@ class TSFresh:
 
         # Filter some features
         impute(extracted_features)
-        features_filtered = select_features(extracted_features, y) # y --> Should be the price that should be predicted
 
         # features_filtered = select_features(extracted_features, timeseries)
         print("IMPUTED")
@@ -73,14 +110,31 @@ if __name__ == "__main__":
 
     # Select a subset of relevant features
 
-    # TODO: Look at 1000 items only
-    df = df.head(1000)
+    df = df.dropna()
 
+    df_sampled = df.head(6000)
+    print("Uniques: ", df_sampled['Label'].unique())
 
-    print(df.head(2))
+    print(df_sampled.head(2))
 
 
     transformer = TSFresh()
-    df_hat = transformer.transform(df)
+
+    # Drop all na's
+    df_sampled = df_sampled.dropna()
+
+    X_df_sampled = df_sampled[['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'OpenInt', 'Label', 'ReturnOpenPrevious']]
+    y_df_sampled = df_sampled['ReturnOpenNext']
+
+    transformer.fit(X_df_sampled, y_df_sampled)
+
+    # Use the real dataset as well
+    df = df.head(100000)
+    X_df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'OpenInt', 'Label', 'ReturnOpenPrevious']]
+    y_df = df['ReturnOpenNext'].values
+
+
+    df_sampled_hat = transformer.transform(X_df_sampled, y_df_sampled)
     # print("Out is: ")
     # print(df_hat.head(2))
+
