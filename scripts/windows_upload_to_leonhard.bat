fb38f1b4cc131f522f1a95c63651138ebb5e2b01
Thomas Greminger <thomas.greminger@gmail.com>
2019-01-23 10:56:33 +0100

    changs to the windows_upload_to_leonhard.bat

--- scripts/windows_upload_to_leonhard.bat
+++ scripts/windows_upload_to_leonhard.bat
@@ -1,4 +1,6 @@
-cd C:\Users\Thomas\Desktop\deeplearning
-rsync -avzh -P --stats --timeout=60 --exclude=.git --exclude=.idea . grethoma@login.leonhard.ethz.ch:~/deeplearning/
+REM  REMOVE THE '--exclude=/data' the very first time you execute this bash script! --include=/data/processed/all.csv
 
+
+cd C:\Users\Thomas\Desktop\deeplearning
+rsync -avzh -P --stats --timeout=60 --exclude=.git --exclude=/.env  --exclude=.idea --exclude=/data --include=/data/processed/all.csv  . grethoma@login.leonhard.ethz.ch:~/deeplearning/
 cd C:\Users\Thomas\Desktop\deeplearning\scripts
