b357b0a02ebe6faa2577db18b73906e87156d80a
Thomas Greminger <thomas.greminger@gmail.com>
2019-01-23 10:11:16 +0100

    Added the windows version of upload_to_leonhard

---
+++ scripts/windows_upload_to_leonhard.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Thomas\Desktop\deeplearning
+rsync -avzh -P --stats --timeout=60 --exclude=.git --exclude=.idea . grethoma@login.leonhard.ethz.ch:~/deeplearning/
+
+cd C:\Users\Thomas\Desktop\deeplearning\scripts
